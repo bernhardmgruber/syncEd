c59456d96b5d5c52957b5822dc9b3b35fa3e3de8
dixxi <dixxi1@gmx.net>
2014-12-19 02:56:51 +0100

    improved peer color function

--- src/SyncEd.Editor/PeerColorExtension.cs
+++ src/SyncEd.Editor/PeerColorExtension.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -12,8 +13,27 @@ namespace SyncEd.Editor
 	{
 		public static Color Color(this Peer peer)
 		{
-			int hash = peer.Address.GetHashCode();
-			return new Color() { R = (byte)(hash >> 16), G = (byte)(hash >> 8), B = (byte)(hash >> 0), A = 0xFF };
+			int address = peer.Address.Address.GetHashCode();
+			short port = (short)peer.Address.Port;
+
+			short a = (short)((short)address +(short)(address >> 16));
+			byte a1 = (byte)(a >> 8);
+			byte a2 = (byte)(a >> 0);
+			byte p = (byte)((byte)port + (byte)(port >> 8));
+
+			// from https://social.msdn.microsoft.com/Forums/vstudio/en-US/9f52904e-dc12-4235-ad86-b691f6b91229/reverse-bits-in-byte-question?forum=csharpgeneral
+			Func<byte, byte> reverse = b => {
+				int rev = (b >> 4) | ((b & 0xf) << 4); 
+				rev = ((rev & 0xcc) >> 2) | ((rev & 0x33) << 2); 
+				rev = ((rev & 0xaa) >> 1) | ((rev & 0x55) << 1); 
+				return (byte)rev; 
+			};
+
+			a1 = reverse(a1);
+			a2 = reverse(a2);
+			p = reverse(p);
+
+			return new Color() { R = a1, G = a2, B = p, A = 0xFF };
 		}
 	}
 }
